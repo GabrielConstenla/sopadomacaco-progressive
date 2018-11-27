72e8e1a494cff9f6097b96101b8433b065be4eff
Gabriel Constenla <ga.constenla@alumnos.duoc.cl>
2018-11-27 13:14:43 -0300

    Primer commit

---
+++ servidor/apiperris/api/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+ESTADO = (
+        ('ADOPTADO','ADOPTADO'),
+        ('RESCATADO', 'RESCATADO'),
+        ('DISPONIBLE','DISPONIBLE'),
+    )
+
+class Adoptado(models.Model):
+    id = models.AutoField( primary_key = True )
+	fotografiaUrl = models.CharField( max_length = 255, default = '' )
+	nombre = models.CharField(max_length=50)
+	raza = models.CharField(max_length=30)
+	descripcion = models.TextField()
+	estado = models.CharField(max_length=10, choices=ESTADO)
+	def __str__(self):
+		return self.nombre
