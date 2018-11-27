95723f7d107217cbbd359152cf01f8e1df88e42d
Gabriel Constenla <ga.constenla@alumnos.duoc.cl>
2018-11-27 17:35:24 -0300

    Se agrega app para cliente
    Se aÃaden los html para el cliente junto con sus css y js
    correspondientes(versiones anteriores). Se aÃade manifest y service
    worker.

---
+++ cliente/service-worker.js
@@ -0,0 +1,85 @@
+var cacheName = 'misperris-v5';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/galeria.html',
+  '/adopta.html',
+  '/css/carrusel.css',
+  '/css/galerias_style.css',
+  '/css/menu.css',
+  '/css/style.css',
+  '/img/familia3-1024.jpg',
+  '/img/familia4-1024.jpg',
+  '/img/familia5-1024.jpg',
+  '/img/batdog.jpg',
+  '/img/dogbin.jpg',
+  '/img/spiderguau.jpg',
+  '/img/SrGuaustark.jpg',
+  '/img/perromain2.0.png',
+  '/img/adopta-300.jpg',
+  '/img/logoPerris.png',
+  '/js/carrusel.js',
+  '/js/extras.js',
+  '/js/validation.js',
+  '/js/app.js'
+];
+
+var dataCacheName = 'misperrisData-v1'
+
+self.addEventListener( 'install', function( e ) {
+    console.log( '[ServiceWorker] Install' );
+    e.waitUntil(
+        caches.open( cacheName ).then( function( cache ) {
+            console.log( '[ServiceWorker] Caching app shell' );
+            return cache.addAll( filesToCache );
+        } )
+    );
+} );
+
+self.addEventListener( 'activate', function( e ) {
+    console.log( '[ServiceWorker] Activate' );
+    e.waitUntil(
+        caches.keys( ).then( function( keyList ) {
+            return Promise.all( keyList.map( function( key ) {
+                if ( key != cacheName ) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete( key );
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+  var dataUrl = "http://127.0.0.1:8000/Adoptado/";
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    /*
+     * When the request URL contains dataUrl, the app is asking for fresh
+     * weather data. In this case, the service worker always goes to the
+     * network and then caches the response. This is called the "Cache then
+     * network" strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+     */
+    e.respondWith(
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+    /*
+     * The app is asking for app shell files. In this scenario the app uses the
+     * "Cache, falling back to the network" offline strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+     */
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
+});
