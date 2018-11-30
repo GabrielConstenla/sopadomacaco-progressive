3f46b4942b91b35833072617f71c8c4dfd782ca7
Gabriel Constenla Torres <43919147+GabrielConstenla@users.noreply.github.com>
2018-11-30 13:58:28 -0300

    se añade url a whitelist

--- servidor/apiperris/apiperris/settings.py
+++ servidor/apiperris/apiperris/settings.py
@@ -131,4 +131,14 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_REGEX_WHITELIST = (
     "http://127.0.0.1:8887",
     "https://gaperris2.pythonanywhere.com/Perro/",
+    "https://gabrielconstenla.github.io",
 )
+
+CORS_ORIGIN_WHITELIST = (
+    "http://127.0.0.1:8887",
+    "https://gaperris2.pythonanywhere.com/Perro/",
+    "https://gabrielconstenla.github.io",
+)
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
